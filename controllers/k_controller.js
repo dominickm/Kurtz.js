4fff540608ce1620ac2760ec6295baba792615ac
Michael Dominick <michael@themadbotter.com>
2018-11-03 22:47:30 -0400

    Incremental -- working on basical controller base class

---
+++ controllers/k_controller.js
@@ -0,0 +1,10 @@
+class KController {
+    constructor(modelRestName) {
+        this.modelRestName= modelRestName;
+    }
+
+    getAll() {
+        // literally should never work but hey testing 
+
+    }
+}
