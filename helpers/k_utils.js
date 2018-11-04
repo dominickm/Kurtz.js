916e8e1f193c7386d6777665b57d0288c144359f
Michael Dominick <michael@themadbotter.com>
2018-11-03 22:34:26 -0400

    isReally Null has been wrapped in a KUtils class for scoping and to prevent future complaints

--- helpers/k_utils.js
+++ helpers/k_utils.js
@@ -1,9 +1,10 @@
 // These are global functions that can be used anywhere 
-
-function isReallyNull(something) {
-    var isNull = false;
-    if (something == null || something === undefined || something === "") {
-        isNull = true;
+class KUtils {
+    static isReallyNull(something) {
+        var isNull = false;
+        if (something == null || something === undefined || something === "") {
+            isNull = true;
+        }
+        return isNull;
     }
-    return isNull;
 }
