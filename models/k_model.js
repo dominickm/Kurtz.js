8d2fd487b0862634cdfa29201e70c838c4d9fc58
Michael Dominick <michael@themadbotter.com>
2018-11-03 22:39:29 -0400

    Adding basic param checking to KModel.findById

--- models/k_model.js
+++ models/k_model.js
@@ -1,3 +1,5 @@
+import {KUtils} from "../helpers/k_utils.js";
+
 class KModel {
     constructor(id, tableName) {
         this.id = id;
@@ -8,5 +10,11 @@ class KModel {
 
     static findById(id, tableName) {
         // TO DO Implement search local cache for object with Id, then search remote for TS change 
+        if (KUtils.isReallyNull(tableName)) {
+            throw("You must supply a table name.");
+        }
+        if (KUtils.isReallyNull(id)) {
+            throw("You must supply an id");
+        }
     }
 }
