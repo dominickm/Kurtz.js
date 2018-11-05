9005a479595366694c7cd9b8f94a07d53e5f5f73
Michael Dominick <michael@themadbotter.com>
2018-11-04 19:38:28 -0500

    Incremental working on k_controller

--- controllers/k_controller.js
+++ controllers/k_controller.js
@@ -1,3 +1,5 @@
+import { KUtils } from "../helpers/k_utils";
+
 export class KController {
     constructor(modelRestName) {
         this.modelRestName= modelRestName;
@@ -5,5 +7,21 @@ export class KController {
 
     getAll() {
         // literally should never work but hey testing 
+    }
+
+    getById(id) {
+        if (KUtils.isReallyNull(id)) {
+            throw("'id' is required");
+        } else {
+            // find the item on the remote or in cache
+        }
+    }
+
+    post(item) {
+        if (KUtils.isReallyNull(item) || KUtils.isReallyNull(item.id)) {
+            throw("an item with an 'id' is required");
+        } else {
+            // attempt the post request 
+        }
     }
 }
